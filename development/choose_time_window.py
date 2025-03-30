f43887449ff6a458becc4fece75f6cb4a1a003ec
Guat Teo <mtanmath@gmail.com>
2025-03-30 15:27:35 +1100

    add files to development folder

---
+++ development/choose_time_window.py
@@ -0,0 +1,19 @@
+def main():
+    ...
+
+def choose_time_window():
+    """use the swanHill dataset for testing
+    function has 3 arguments, data, time_start, time_end
+    ? use library datetime
+
+    """
+    ...
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
